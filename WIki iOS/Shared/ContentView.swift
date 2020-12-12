c5c526e7247beadcf4b673becc1cf2ee6a2c34ea
Jacob Tepperman <jtepp@icloud.com>
2020-12-12 11:38:34 -0500

    Started iOS app

---
+++ WIki iOS/Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jacob Tepperman on 2020-12-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@State var output = ""
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WIki iOS/Shared/WIki_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WIki_iOSApp.swift
+//  Shared
+//
+//  Created by Jacob Tepperman on 2020-12-12.
+//
+
+import SwiftUI
+
+@main
+struct WIki_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
